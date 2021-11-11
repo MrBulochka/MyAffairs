8450d5f0cce95528beded3b537080d9d2260baf6
MrBulochka_github <xlogan@bk.ru>
2021-11-11 21:48:33 +0300

    Initial commit

---
+++ app/src/main/java/com/notacompany/myaffairs/ui/projects/AddProjectViewModel.kt
@@ -0,0 +1,25 @@
+package com.notacompany.myaffairs.ui.projects
+
+import androidx.lifecycle.*
+import com.notacompany.myaffairs.data.model.Project
+import com.notacompany.myaffairs.data.repository.DataRepository
+import kotlinx.coroutines.launch
+
+class AddProjectViewModel(private val repository: DataRepository): ViewModel() {
+
+    fun insert(project: Project) {
+        viewModelScope.launch {
+            repository.insert(project)
+        }
+    }
+}
+
+class AddProjectViewModelFactory(private val repository: DataRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AddProjectViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return AddProjectViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
